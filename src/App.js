be0a6eb9cbc30fd4969a5929fe786cd91170aa16
Pavel Nyukalo <PashaOktan07@gmail.com>
2021-03-27 13:55:08 +0500

    starts implementing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Quiz />
+      </Layout>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Quiz.css';
+
+import ActiveQuiz from '../../compoments/ActiveQuiz/ActiveQuiz'
+
+class Quiz extends React.Component {
+  state = {
+    quiz: []
+  }
+
+  render() {
+    return (
+      <div className='Quiz'>
+        <div className='QuizWrapper'>
+          <h1>Quiz</h1>
+          <ActiveQuiz />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Quiz
